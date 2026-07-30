57058f5d513f52ac4c7631dc92a428772c46fab6
pimaxpro <dducpimax@gmail.com>
2026-07-30 22:06:19 +0700

    Add Marathon Week 4 exam answers data

---
+++ assets/js/data/answers/marathon/Marathon Week 4.js
@@ -0,0 +1,45 @@
+/* =========================================================
+   ĐÁP ÁN: MARATHON WEEK 4 (Dạng Multi-Section)
+========================================================= */
+window.EXAM_ANSWERS_BANK = window.EXAM_ANSWERS_BANK || {};
+
+window.EXAM_ANSWERS_BANK["Marathon Week 4"] = {
+  "examTitle": "Marathon Week 4",
+  "sections": [
+    {
+      "sectionName": "Multiple Choice",
+      "answers": {
+        "1": "A",
+        "2": "C",
+        "3": "B",
+        "4": "B"
+      }
+    },
+    {
+      "sectionName": "True/False Questions",
+      "answers": {
+        "1": {
+          "a": "Đúng",
+          "b": "Đúng",
+          "c": "Đúng",
+          "d": "Sai"
+        },
+        "2": {
+          "a": "Sai",
+          "b": "Đúng",
+          "c": "Đúng",
+          "d": "Đúng"
+        }
+      }
+    },
+    {
+      "sectionName": "Short Answer Questions",
+      "answers": {
+        "1": "2018",
+        "2": "10200",
+        "3": "8",
+        "4": "464"
+      }
+    }
+  ]
+};
