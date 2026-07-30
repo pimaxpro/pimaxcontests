d8106473d828e032c0c595eb5d715398ed5e0b2d
pimaxpro <dducpimax@gmail.com>
2026-07-30 17:46:42 +0700

    Create Marathon Week 2.js

---
+++ assets/js/data/answers/marathon/Marathon Week 2.js
@@ -0,0 +1,45 @@
+/* =========================================================
+   ĐÁP ÁN: EXAM_CODE (Dạng Multi-Section)
+========================================================= */
+window.EXAM_ANSWERS_BANK = window.EXAM_ANSWERS_BANK || {};
+
+window.EXAM_ANSWERS_BANK["EXAM_CODE"] = {
+  "examTitle": "EXAM_CODE",
+  "sections": [
+    {
+      "sectionName": "Phần trắc nghiệm",
+      "answers": {
+        "1": "C",
+        "2": "B",
+        "3": "B",
+        "4": "C"
+      }
+    },
+    {
+      "sectionName": "Phần đúng/sai",
+      "answers": {
+        "1": {
+          "a": "Đúng",
+          "b": "Đúng",
+          "c": "Đúng",
+          "d": "Đúng"
+        },
+        "2": {
+          "a": "Đúng",
+          "b": "Đúng",
+          "c": "Sai",
+          "d": "Đúng"
+        }
+      }
+    },
+    {
+      "sectionName": "Phần trả lời ngắn",
+      "answers": {
+        "1": "39",
+        "2": "0,35",
+        "3": "200",
+        "4": "0,12"
+      }
+    }
+  ]
+};
